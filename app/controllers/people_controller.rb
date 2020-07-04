42c08d90749923c0a111247b6c6b4875ba1bbe1a
Andrew <andrew.vinicius@ieee.org>
2020-07-04 12:50:38 -0300

    Versão 0.2 Create Reade Update Delete

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,14 +7,27 @@ class PeopleController < ApplicationController
   end
 
   def create
+    person = Person.new(params_people)
+    person.save
+    redirect_to people_index_path
   end
 
   def edit
   end
 
   def update
+    Person.find(params[:id]).update(params_people)
+    redirect_to people_index_path
   end
 
   def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_index_path
   end
+
+  private
+  def params_people
+    params.require(:person).permit(:name, :age)
+  end
+
 end
