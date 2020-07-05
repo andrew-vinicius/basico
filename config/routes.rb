72d456c3b3f4cdcfe914c3cc743c3473a1a9d1d7
Andrew <andrew.vinicius@ieee.org>
2020-07-05 12:23:49 -0300

    Versão 0.3 Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   get 'people/edit'
   get 'people/update'
   get 'people/destroy'
-  get 'home/index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'home#index'
+  root 'people#index'
 
   resources :people
 
